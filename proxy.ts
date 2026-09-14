2f4a0b3921d7248dd77cca4cf9edc8e1c62b7243
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-14 12:24:32 +0530

    rename middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest,NextResponse } from "next/server";
 import { verifyToken } from "./lib/auth";
 
 
-export function middleware(req:NextRequest){
+export function proxy(req:NextRequest){
     const token =req.cookies.get("token")?.value;
 
     if(!token){
