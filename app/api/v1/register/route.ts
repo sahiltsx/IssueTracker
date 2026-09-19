db9f2bd709991ed47ac9432fbf3411644235e850
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-19 23:19:56 +0530

    change the route path

--- app/api/v1/register/route.ts
+++ app/api/v1/register/route.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/db";
 import { NextRequest ,NextResponse } from "next/server";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 
 export async function POST(req:NextRequest) {
@@ -38,15 +39,32 @@ export async function POST(req:NextRequest) {
                      password:hashPassword
                  }
              });
+
+             const token=jwt.sign(
+                {UserId:User.id,email:User.email},
+                process.env.JWT_SECRET as string,
+                {expiresIn:"7d"}
+             )
      
-             return NextResponse.json({
+            const response= NextResponse.json({
                  message:"User created successfully",
                  userId:User.id
              },{
                  status:201
              })
 
-     } catch (error) {
+             response.cookies.set("token",token,{
+                httpOnly:true,
+                secure:process.env.NODE_ENV==="production",
+                sameSite:"lax",
+                maxAge:60*60*24*7,
+                path:"/"
+             });
+
+             return response
+
+     }
+      catch (error) {
         console.log(error)
         return NextResponse.json({
             message:"Internal server error"
