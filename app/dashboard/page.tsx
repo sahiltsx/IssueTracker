f131114d08b461e1b44a736c10d89aeafa058b4f
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-24 21:28:14 +0530

    change something

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,18 +2,16 @@ import { Button } from "@/components/ui/button";
 
 export default function dashboard() {
     return (
-        <div className="max-w-md mx-auto p-6 ">
-           <header className="flex items-center justify-center pb-5 border-b">
+      <div className="max-w-5xl mx-auto pt-4">
+        <header className="flex items-center justify-between pb-5 border border-b-2 border-neutral-500">
             <div>
-                 <h2 className="font-semibold text-2xl">Issue tracker</h2>
+                <h2 className="text-2xl font-semibold">Issue tracker</h2>
             </div>
-           
-           </header>
-
-           <div className="flex gap-3 pt-4">
-             <Button variant="outline">Recently created</Button>
-             <Button variant="outline">Recently updated</Button>
-           </div>
-        </div>
+            <div className="flex gap-3">
+                <Button>New Issue</Button>
+                <h3>User profile</h3>
+            </div>
+        </header>
+      </div>
     )
 }
