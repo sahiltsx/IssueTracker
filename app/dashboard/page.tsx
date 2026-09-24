114727bee19fb7510c21b1b046c178fc2b271bb9
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-24 21:44:03 +0530

    make a header part

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,18 @@
 import { Button } from "@/components/ui/button";
 
-export default function dashboard() {
+export default function Dashboard() {
     return (
-      <div className="max-w-5xl mx-auto pt-4">
-        <header className="flex items-center justify-between pb-5 border border-b-2 border-neutral-500">
-            <div>
-                <h2 className="text-2xl font-semibold">Issue tracker</h2>
-            </div>
-            <div className="flex gap-3">
-                <Button>New Issue</Button>
-                <h3>User profile</h3>
-            </div>
-        </header>
+      <div className="min-h-screen">
+        <div className="max-w-7xl mx-auto pt-3">
+            <header className="flex items-center justify-between pb-4 border-b">
+                <div className="flex items-center ml-2">
+                    <h2 className="text-2xl font-semibold tracking-tight">Issue Tracker</h2>
+                </div>
+                <div className="flex items-center gap-2 mr-4">
+                    <Button variant="outline" className=" text-sm font-medium">New Issue</Button>
+                </div>
+            </header>
+        </div>
       </div>
     )
 }
