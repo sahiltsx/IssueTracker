9c9532af72cc41f3293c683a50674417a7b62cda
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-24 21:07:30 +0530

    initial commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,19 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function dashboard() {
     return (
-        <div>
-            <h1>DashBoard Page !</h1>
-            <h2>Welcome to Issue tracker</h2>
+        <div className="max-w-md mx-auto p-6 ">
+           <header className="flex items-center justify-center pb-5 border-b">
+            <div>
+                 <h2 className="font-semibold text-2xl">Issue tracker</h2>
+            </div>
+           
+           </header>
+
+           <div className="flex gap-3 pt-4">
+             <Button variant="outline">Recently created</Button>
+             <Button variant="outline">Recently updated</Button>
+           </div>
         </div>
     )
 }
