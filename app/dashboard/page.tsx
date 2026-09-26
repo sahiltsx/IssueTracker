25c99eefe240d31b224e1bdeeb326305eecd86bb
Sahil Sharma <sahilsharma901590@gmail.com>
2026-09-26 16:38:16 +0530

    change in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,6 @@ import { Input } from "@/components/ui/input";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Sheet,SheetContent,SheetHeader,SheetTitle } from "@/components/ui/sheet";
 
-
 const DUMMY_ISSUES = [
   {
     id: "1",
